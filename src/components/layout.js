ab22eb2cde9676e23d9623bed49fdf904bc4f4af
OliverHuntsman <admin@gemmasutton.com>
2021-12-15 19:13:09 +0000

    remove elemnts from the menu - commented out

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,9 +33,9 @@ const Layout = props => {
               <li className="nav-about" role="menuitem">
                 <Link to={`/about`}>About</Link>
               </li>
-              <li className="nav-elements" role="menuitem">
+              {/* <li className="nav-elements" role="menuitem">
                 <Link to={`/elements`}>Elements</Link>
-              </li>
+              </li> */}
               <li className="nav-tags" role="menuitem">
                 <Link to={`/tags`}>Tags</Link>
               </li>
